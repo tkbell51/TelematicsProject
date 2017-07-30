e0f38215a4135afd4747478a8f2586943615c8e1
tkbell51 <tkbell51@gmail.com>
2017-07-29 23:59:30 -0400

    back to object almost complete

--- src/com/tbell/TelematicService.java
+++ src/com/tbell/TelematicService.java
@@ -1,30 +1,50 @@
 package com.tbell;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class TelematicService {
+    private VehicleInfo vehicleInfo;
 
     public TelematicService(){};
 
     public void report(VehicleInfo vehicleInfo){
-int vin = vehicleInfo.getVIN();
-String filename = vin + ".json";
+
         try {
+            int vin = vehicleInfo.getVIN();
+            String filename = vin + ".json";
+
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(vehicleInfo);
+
             File file = new File(filename);
             FileWriter fileWriter = new FileWriter(file);
-            fileWriter.write("VIN: " + vehicleInfo.getVIN() + "\n");
-            fileWriter.write("Odometer: " + vehicleInfo.getOdometer() + "\n");//Very simple!
-            fileWriter.write("Consumption: " + vehicleInfo.getConsumption() + "\n");
-            fileWriter.write("Last Oil Change OdometerReading: " + vehicleInfo.getLastOilChange() + "\n");
-            fileWriter.write("Engine Size: " + vehicleInfo.getEngineSize() + "\n");
-            fileWriter.close(); //close() cleans up and commits changes
-        } //If Java doesn't find the file it will create it for us
-        catch (IOException ex) { //A general exception that covers many errors
+            fileWriter.write(json);
+            fileWriter.close();
+
+        }
+        catch (IOException ex) {
             ex.printStackTrace();
             System.out.println("File not created.");
         }
-    }
 
+    }
+    public void backToObject () throws IOException{
+        File file = new File(".");
+        VehicleInfo vi = null;
+        for (File f : file.listFiles()) {
+            if (f.getName().endsWith(".json")) {
+                new FileReader(f);
+                ObjectMapper mapper = new ObjectMapper();
+                vi = mapper.readValue(f, VehicleInfo.class);
+
+            }
+        }
+    }
 }
+
+
